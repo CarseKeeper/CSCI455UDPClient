cc8ad6b12b7d7dab9b70a803917776a813be92b0
CarseKeeper <125390842+CarseKeeper@users.noreply.github.com>
2023-10-31 01:33:08 -0500

    Did some stuff

--- Client.java
+++ Client.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.text.DecimalFormat;
 import java.text.Format;
@@ -20,19 +24,24 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            WriteJsonObject json = new WriteJsonObject(); //Serializer and Deserializer object
+
+            WriteJsonObject json = new WriteJsonObject(); // Serializer and Deserializer object
             Scanner scan = new Scanner(System.in); // Scanner for client input
             ArrayList<Event> EVENTS = new ArrayList<Event>(); // List of all Events from the database
-            Socket server;
-            BufferedReader in; //from server
-            DataOutputStream out; //to server
+            DatagramSocket server;
+            DatagramSocket client;
+            InetAddress Ip;
+            byte buf[];
+            int port;
 
             // While loop for getting a connection to the server
             while (true) {
                 try {
-                    server = connectToServer(); //Method prompts for host ip and port
-                    out = new DataOutputStream(server.getOutputStream()); //dataouputstream created from socket
-                    in = new BufferedReader(new InputStreamReader(server.getInputStream())); //bufferedreader created from socket
+                    server = connectToServer(); // Method prompts for host ip and port
+                    client = connectToServer();
+                    Ip = InetAddress.getByName(getHost());
+                    buf = null;
+                    port = getPort();
                     break;
 
                 } catch (Exception e) {
@@ -40,17 +49,18 @@ public class Client {
                 }
             }
 
-            //MAIN Logic Loop
+            // MAIN Logic Loop
             while (true) {
-                //User is prompted for an action
-                System.out.printf("%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s\n> ",
+                // User is prompted for an action
+                System.out.printf(
+                        "%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s%n%-4s    %-30s\n> ",
                         "(1):", "List the current events", "(2):", "Create a new event", "(3):", "Donate to an event",
                         "(4):", "Update an event", "(5):", "List ALL events", "(q):", "Quit");
                 String answer = scan.nextLine();
 
                 // LIST CURRENT EVENTS
                 if (answer.startsWith("1")) {
-                    EVENTS = getEvents(in, out);
+                    EVENTS = getEvents(server, buf, Ip, port);
                     EVENTS = currentEvents(EVENTS);
                     listEvents(EVENTS);
                     System.out.println();
@@ -58,22 +68,22 @@ public class Client {
                 // CREATE A NEW EVENT
                 else if (answer.startsWith("2")) {
                     CreateEventRequest newEvent = formEvent();
-                    createEvent(newEvent, out, in, json);
+                    // createEvent(newEvent, out, in, json);
                 }
                 // DONATE AN AMOUNT TO AN EVENT
                 else if (answer.startsWith("3")) {
-                    EVENTS = getEvents(in, out);
-                    donateToEvent(EVENTS, in, out, json);
+                    // EVENTS = getEvents(in, out);
+                    // donateToEvent(EVENTS, in, out, json);
                 }
                 // UPDATES AN EVENT FROM ALL EVENTS
                 else if (answer.startsWith("4")) {
-                    EVENTS = getEvents(in, out);
+                    // EVENTS = getEvents(in, out);
                     listEvents(EVENTS);
-                    updateEvent(EVENTS, in, out, json);
+                    // updateEvent(EVENTS, in, out, json);
                 }
                 // PRINTS OUT CURRENT EVENTS AND PAST EVENTS
                 else if (answer.startsWith("5")) {
-                    EVENTS = getEvents(in, out);
+                    // EVENTS = getEvents(in, out);
                     listAllEvents(EVENTS);
                     System.out.println();
                 }
@@ -87,7 +97,7 @@ public class Client {
                 }
             }
 
-            //Always close the connection when done
+            // Always close the connection when done
             server.close();
         } catch (Exception e) {
             System.err.println(e);
@@ -139,20 +149,20 @@ public class Client {
     private static CreateEventRequest formEvent() {
         Scanner scan = new Scanner(System.in);
         String title = null;
-        String description =null;
+        String description = null;
         double target = -1;
         String deadline = null;
         try {
-            while(title == null) {
+            while (title == null) {
                 System.out.print("Enter the title for Event: ");
                 title = scan.nextLine();
             }
-            while(description == null) {
+            while (description == null) {
                 System.out.print("Enter the description for the Event: ");
 
                 description = scan.nextLine();
             }
-            while(target < 0) {
+            while (target < 0) {
                 System.out.print("Enter the target amount as a double (####.##): ");
                 String temp = scan.nextLine();
                 Scanner tempScan = new Scanner(temp);
@@ -176,9 +186,9 @@ public class Client {
     /**
      * tries to connect to the server
      */
-    private static Socket connectToServer() {
+    private static DatagramSocket connectToServer() {
         try {
-            return new Socket(getHost(), getPort(), null, 0);
+            return new DatagramSocket(6789);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -218,25 +228,48 @@ public class Client {
     /**
      * gets all events from server and returns them as an ArrayList
      */
-    private static ArrayList<Event> getEvents(BufferedReader in, DataOutputStream out) {
+    private static ArrayList<Event> getEvents(DatagramSocket ds, byte buf[], InetAddress Ip, int port) {
         ArrayList<Event> events = new ArrayList<Event>();
         WriteJsonObject json = new WriteJsonObject();
         try {
-            out.writeBytes(json.serialize(new Request(RequestType.EVENTS, json.serialize(new EventsRequest()))) + "\n");
-
+            buf = (json.serialize(new Request(RequestType.EVENTS, json.serialize(new EventsRequest()))) + "\n")
+                    .getBytes();
+            DatagramPacket DpSend = new DatagramPacket(buf, buf.length, Ip, port);
+            ds.send(DpSend);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         try {
-            Response response = json.deserialize(in.readLine(), Response.class);
-            if (response.responseType == RequestType.EVENTS)
-                events = json.deserialize(response.responseBody, new TypeReference<ArrayList<Event>>() {});
+            byte[] recieve = new byte[65535];
+            DatagramPacket DpRecieve = new DatagramPacket(recieve, recieve.length);
+            ds.receive(DpRecieve);
+            System.out.println(DpRecieve.toString());
+            // Response response = json.deserialize(data(recieve).toString(),
+            // Response.class);
+            // if (response.responseType == RequestType.EVENTS)
+            // events = json.deserialize(response.responseBody, new
+            // TypeReference<ArrayList<Event>>() {
+            // });
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
-
         return events;
+    }
+
+    /**
+     * Makes byte array into string
+     */
+    public static StringBuilder data(byte[] a) {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0) {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
     }
 
     /**
@@ -248,7 +281,7 @@ public class Client {
         try {
             System.out.println("choose an event or -1 to go back: ");
             int index = scan.nextInt() - 1;
-            if (index < EVENTS.size()  && index >= 0) {
+            if (index < EVENTS.size() && index >= 0) {
                 Event newEvent = EVENTS.get(index);
                 String update = changeEvent(newEvent, json);
                 out.writeBytes(json.serialize(new Request(RequestType.UPDATE, update)) + "\n");
@@ -274,7 +307,7 @@ public class Client {
             listEvents(curEvents);
             int index = -2;
             String temp;
-            while (index < -1){
+            while (index < -1) {
                 System.out.print("Choose and active event or 0 to go back: ");
                 temp = scan.nextLine();
                 Scanner scanTemp = new Scanner(temp);
@@ -317,7 +350,7 @@ public class Client {
     private static String changeEvent(Event event, WriteJsonObject json) {
         Scanner scan = new Scanner(System.in);
         String title = null;
-        while (title == null){
+        while (title == null) {
             System.out.printf("Current Title: %25s%n", event.getTitle());
             System.out.print("New Title: ");
             title = scan.nextLine();
@@ -326,7 +359,7 @@ public class Client {
             event.setTitle(title);
 
         String description = null;
-        while(description == null) {
+        while (description == null) {
             System.out.printf("Current Description: %s%n", event.getDescription());
             System.out.print("New Description: ");
             description = scan.nextLine();
@@ -337,15 +370,15 @@ public class Client {
         double target = -1.0;
         String temp = "";
 
-        while(target < 0) {
+        while (target < 0) {
             System.out.printf("Current Target: %-15s%n", getCurrency(event.getTarget()));
             System.out.print("New Target: ");
             temp = scan.nextLine();
             Scanner scantemp = new Scanner(temp);
-            if(temp.isEmpty()){
+            if (temp.isEmpty()) {
                 break;
             }
-            if(scantemp.hasNextDouble())
+            if (scantemp.hasNextDouble())
                 target = scantemp.nextDouble();
         }
         if (!temp.isEmpty())
@@ -353,22 +386,22 @@ public class Client {
 
         double balance = -1.0;
 
-        while(balance < 0) {
+        while (balance < 0) {
             System.out.printf("Current Balance: %-15s%n", getCurrency(event.getBalance()));
             System.out.print("New Balance: ");
             temp = scan.nextLine();
             Scanner scantemp = new Scanner(temp);
-            if(temp.isEmpty()){
+            if (temp.isEmpty()) {
                 break;
             }
-            if(scantemp.hasNextDouble())
+            if (scantemp.hasNextDouble())
                 balance = scantemp.nextDouble();
         }
         if (!temp.isEmpty())
             event.setCurrentPool(balance);
 
         String date = null;
-        while(date == null) {
+        while (date == null) {
             System.out.printf("Current Date: %-20s%n", event.getDeadlineString());
             System.out.print("New Date (YYYY-MM-DDTHH:MM:ss.SSSZ): ");
             date = scan.nextLine();
@@ -384,19 +417,24 @@ public class Client {
     private static void listEvents(ArrayList<Event> events) {
         int i = 1;
         for (Event event : events) {
-            System.out.printf("%8d.    %-35s  Ends: %-12s%n               = %-1s =%n----------------------------------------------------%n", i++, event.getTitle(), event.getDeadlineString().substring(0,10), event.getDescription());
+            System.out.printf(
+                    "%8d.    %-35s  Ends: %-12s%n               = %-1s =%n----------------------------------------------------%n",
+                    i++, event.getTitle(), event.getDeadlineString().substring(0, 10), event.getDescription());
         }
     }
 
     /**
      * Prints all events, past and current, in a table
      */
-    private static void listAllEvents(ArrayList<Event> EVENTS){
+    private static void listAllEvents(ArrayList<Event> EVENTS) {
         ArrayList<Event> curEvents = currentEvents(EVENTS);
         ArrayList<Event> pastEvents = pastEvents(EVENTS);
-        System.out.println("--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
-        System.out.printf("|  %-25s | %-15s | %-8s | %-12s  |=|  %-25s | %-15s | %-8s | %-12s  |%n", "Current Events", "Target", "Percent", "Deadline", "Past Events", "Target", "Percent", "Deadline");
-        System.out.println("--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
+        System.out.println(
+                "--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
+        System.out.printf("|  %-25s | %-15s | %-8s | %-12s  |=|  %-25s | %-15s | %-8s | %-12s  |%n", "Current Events",
+                "Target", "Percent", "Deadline", "Past Events", "Target", "Percent", "Deadline");
+        System.out.println(
+                "--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
         for (int i = 0; i < curEvents.size() || i < pastEvents.size(); i++) {
             String ce = "";
             String cet = "";
@@ -419,18 +457,21 @@ public class Client {
                 ped = pastEvents.get(i).getDeadlineString().substring(0, 10);
             }
 
-            System.out.printf("|  %-25s | %-15s | %-8s | %-12s  |=|  %-25s | %-15s | %-8s | %-12s  |%n", ce, cet, cep, ced, pe, pet, pep, ped);
-            System.out.println("--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
+            System.out.printf("|  %-25s | %-15s | %-8s | %-12s  |=|  %-25s | %-15s | %-8s | %-12s  |%n", ce, cet, cep,
+                    ced, pe, pet, pep, ped);
+            System.out.println(
+                    "--------------------------------------------------------------------------|=|--------------------------------------------------------------------------");
 
         }
         System.out.println();
     }
 
     /**
-     * gets the percent completeness of an event and returns it as a String with 2 decimal places
+     * gets the percent completeness of an event and returns it as a String with 2
+     * decimal places
      */
-    private static String getPercent(double target, double balance){
-        double percent = (balance/target);
+    private static String getPercent(double target, double balance) {
+        double percent = (balance / target);
         NumberFormat percentage = NumberFormat.getPercentInstance();
         return percentage.format(percent);
     }
@@ -438,7 +479,7 @@ public class Client {
     /**
      * gets the currency formatted String of a value
      */
-    private static String getCurrency(double value){
+    private static String getCurrency(double value) {
         DecimalFormat currency = new DecimalFormat("$###,##0.00");
         return currency.format(value);
     }
